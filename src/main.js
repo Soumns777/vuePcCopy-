8ac682906a9cc73833d15141da7b9bbafff737ab
soumns <2794122209@qq.com>
2022-05-25 17:41:55 +0800

    ✨ feat:change-init

--- src/main.js
+++ src/main.js
@@ -17,34 +17,31 @@ Vue.component('tree-table', TreeTable)
 // TODO 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 
-import 'quill/dist/quill.core.css' // import styles
-import 'quill/dist/quill.snow.css' // for snow theme
-import 'quill/dist/quill.bubble.css' // for bubble theme
+// import 'quill/dist/quill.core.css' // import styles
+// import 'quill/dist/quill.snow.css' // for snow theme
+// import 'quill/dist/quill.bubble.css' // for bubble theme
 
 Vue.use(VueQuillEditor)
 
 // TODO  导入lodash
-var _ = require('lodash');
+var _ = require('lodash')
 
 // TODO 导入nprogress (css也要加入)
 import nprogress from 'nprogress'
 import '../node_modules/nprogress/nprogress.css'
 
-
-
-
 // TODO 全局导入 axios
-import axios from 'axios';
+import axios from 'axios'
 
 // TODO 基地址 (需要在挂载之前)
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 // TODO 将 axios当做一个属性挂载给 vue 的函数的原型上,这样在其他文件中也能发送 axios 请求
-Vue.prototype.$http = axios;
+Vue.prototype.$http = axios
 
-//.FIXME: 请求拦截器挂载 token令牌 
-axios.interceptors.request.use(function(config) {
-
+//.FIXME: 请求拦截器挂载 token令牌
+axios.interceptors.request.use(
+  function (config) {
     //.TODO:  nprogress.start() 加载动画
     nprogress.start()
 
@@ -52,21 +49,19 @@ axios.interceptors.request.use(function(config) {
 
     // TODO  手动获取 token令牌
     config.headers.Authorization = sessionStorage.getItem('token')
-    return config;
-}, function(error) {
-    return Promise.reject(error);
-});
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 
 //.FIXME: 响应拦截器
-axios.interceptors.response.use(function(config) {
-
-    //.TODO: 加载动画
-    nprogress.done();
-    return config
+axios.interceptors.response.use(function (config) {
+  //.TODO: 加载动画
+  nprogress.done()
+  return config
 })
-
-
-
 
 //.FIXME: 在全局中定义过滤时间的过滤器  (将事件戳转换为 年月日)
 
@@ -99,25 +94,20 @@ axios.interceptors.response.use(function(config) {
 
 // })
 
-
-
-// 
-
+//
 
 //.FIXME: 导入 moment.js 制作过滤器
 
 import moment from 'moment'
-Vue.filter('dateFormate', function(val) {
-    return (moment(val).format('YYYY-MM-DD HH:mm:ss'))
+Vue.filter('dateFormate', function (val) {
+  return moment(val).format('YYYY-MM-DD HH:mm:ss')
 })
-
-
 
 // 去除控制台日志信息
 Vue.config.productionTip = false
 
 // TODO 将路由挂载到vue实例上
 new Vue({
-    router,
-    render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount('#app')
